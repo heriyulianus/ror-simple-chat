b6720af7732b85b3943c79c8d502d5fdf28b8add
heriyulianus <heri4n@gmail.com>
2019-12-13 01:38:14 +0700

    init

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_12_12_133900) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
